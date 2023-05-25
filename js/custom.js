79751c144968a053f14674d8f0ff2a951cd56cd0
Mahmoud Elkomy <82784354+mahmoudelkomy996@users.noreply.github.com>
2023-05-25 14:57:35 +0300

    permissions

---
+++ js/custom.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+   // nav menu
+   $(".navBtn").click(function () {
+    $(this).toggleClass("active");
+    $(".navMenu").toggleClass("active");
+  });
+  //navbar ainmation
+  $(window).scroll(function () {
+    var appScroll = $(document).scrollTop();
+    if (appScroll >= 1) {
+      $("header").addClass("headerAnimate");
+    } else {
+      $("header").removeClass("headerAnimate");
+    }
+  });
+});
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+$(document).ready(function () {
+  // odometer
+  $(".odometer").appear(function (e) {
+    var odo = $(".odometer");
+    odo.each(function () {
+      var countNumber = $(this).attr("data-count");
+      $(this).html(countNumber);
+    });
+  });
+  // select2
+  $(".select2search").select2();
+  $(".select2").select2({
+    minimumResultsForSearch: -1,
+  });
+  // aos
+  AOS.init({
+    offset: 60,
+    delay: 50,
+    duration: 1000,
+    // easing: "linear",
+    once: true,
+  });
+  //aos Delay
+  function aosDelay() {
+    var class_ = "mainSection";
+    $("section").each(function (i) {
+      class_ = $(this).attr("class");
+      if (class_) {
+        $("." + class_ + " div[data-aos]").each(function (i) {
+          var d = 0;
+          d = i * 100;
+          $(this).attr("data-aos-delay", d);
+          d = 0;
+        });
+      }
+    });
+  }
+  aosDelay();
+});
